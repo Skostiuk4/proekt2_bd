464fde9fcd02e903922ed12935ca144d88aacf99
Skostiuk4 <Skostiuk4@gmail.com>
2019-10-15 15:59:57 +0300

    Code editing

--- list.php
+++ list.php
@@ -1,38 +1,34 @@
 <?php
-  if(isset($_POST['search']));
-  else {
-      $query = "SELECT * FROM `users`";
-      $search_result = filterTable($query);
-  }
-  function filterTable($query) {
-      $connect = mysqli_connect("localhost", 'root', '1111', 'resource1');
-      $filter_Result = mysqli_query($connect, $query);
-      return $filter_Result;
-  }
-  ?>
+$query = "SELECT * FROM `users`";
+$search_result = filterTable($query);
+
+function filterTable($query) {
+	$connect = mysqli_connect("localhost", 'root', '1111', 'resource1');
+	$filter_Result = mysqli_query($connect, $query);
+	return $filter_Result;
+}
+?>
 
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"/>
     <title>Document</title>
 </head>
 <body>
-    <form>
-        <table class="table">
-            <thead>
-            <tr>
-                <th>Id</th>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Email</th>
-                <th>Age</th>
-            </tr>
-            <?php while ($row = mysqli_fetch_array($search_result)):?>
+<form>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>First name</th>
+            <th>Email</th>
+            <th>Age</th>
+        </tr>
+								<?php while ($row = mysqli_fetch_array($search_result)):?>
             <tr>
                 <td><?php echo $row['id'];?></td>
                 <td><?php echo $row['first_name'];?></td>
@@ -40,9 +36,9 @@
                 <td><?php echo $row['email'];?></td>
                 <td><?php echo $row['age'];?></td>
             </tr>
-            <?php endwhile;?>
-        </table>
-    </form>
+								<?php endwhile;?>
+    </table>
+</form>
 </body>
 </html>
 
