e4fd10e8307be03a4561ba3f9daae9e712b0a30d
Skostiuk4 <Skostiuk4@gmail.com>
2019-10-15 12:27:08 +0300

    Add table

--- list.php
+++ list.php
@@ -1,29 +1,39 @@
-<?php
-  require_once("bd.php");
-  if($connection  == false) {
-    echo "Error!";
-    echo mysqli_connect_errno();
-    exit();
-  }
-
-?>
 <!doctype html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Document</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"/>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"/>
+    <title>Document</title>
 </head>
 <body>
-
- <?php
- $query = mysqli_query($connection, "SELECT * FROM $bdarticles");
- $numrows = mysqli_num_rows($query);
-  $i = 0;
-    while ($row = mysqli_fetch_assoc($query)) {
-      echo $row['first_name']. '<br>';
-    }
-
- ?>
+<table class="table">
+    <thead>
+    <tr>
+        <th>Id</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Age</th>
+    </tr>
+    </thead>
 </body>
 </html>
+
+<?php
+require_once("bd.php");
+if($connection  == false) {
+  echo "Error!";
+}
+$query = mysqli_query($connection, "SELECT * FROM $bdarticles");
+$numrows = mysqli_num_rows($query);
+$i = 0;
+while ($row = mysqli_fetch_assoc($query)) {
+  echo "<table bordre='1'><th>". $row['first_name']. "</th>" . '<br>';
+}
+
+?>
+
+
